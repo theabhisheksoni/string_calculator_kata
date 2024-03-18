aefa759fab37db9bd945ae0b2a5d811058838ac0
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-03-18 17:22:01 +0530

    Add test case for when given a custom delimiter

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -29,5 +29,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.new.add("1\n2,3")).to eq(6)
       end
     end
+
+    context "when given a custom delimiter" do
+      it "returns the sum of the numbers using the custom delimiter" do
+        expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end

8ddf59154f4a4f7a8e22862c4224bd023479dca7
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-03-18 17:24:27 +0530

    Implemented code for custom delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,25 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
 
-    numbers = string.split(/,|\n/)
+    delimiter = extract_delimiter(string)
+    numbers = extract_numbers(string, delimiter)
     numbers.map(&:to_i).sum
+  end
+
+  private
+
+  def extract_delimiter(string)
+    if string.start_with?("//")
+      string[2]
+    else
+      ','
+    end
+  end
+
+  def extract_numbers(string, delimiter)
+    if string.start_with?("//")
+      string = string.split("\n", 2)[1]
+    end
+    string.split(/#{Regexp.escape(delimiter)}|\n/)
   end
 end

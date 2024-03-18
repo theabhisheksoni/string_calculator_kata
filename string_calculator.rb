8fa1b8e873412060e8aeef0a5c4508b681c9c6e2
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-03-18 15:59:16 +0530

    Implement addition of multiple numbers separated by commas in StringCalculator's add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,11 @@ class StringCalculator
 
   def add(string)
     return 0 if string.empty?
+
+    sum = 0
+    string.split(",").map do |string|
+      sum += string.to_i
+    end
+    sum
   end
 end

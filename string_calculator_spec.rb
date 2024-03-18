223e89fac9ad1fc96253db1b260b9a26d547f2b5
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-03-18 15:53:47 +0530

    Add test case for StringCalculator's add method when given a single number

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe StringCalculator do
         expect(string.add("")).to eq(0)
       end
     end
+
+    context "when given a single number" do
+      it "returns the number" do
+        calculator = StringCalculator.new
+        expect(calculator.add("1")).to eq(1)
+      end
+    end
   end
 end
